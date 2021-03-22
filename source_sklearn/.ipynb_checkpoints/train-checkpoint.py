f49f57f23195f9d147cc02df6dffe56d3dc84ee8
EC2 Default User <ec2-user@ip-172-16-171-146.ec2.internal>
2021-03-22 11:54:19 +0000

    project completion

--- source_sklearn/.ipynb_checkpoints/train-checkpoint.py
+++ source_sklearn/.ipynb_checkpoints/train-checkpoint.py
@@ -5,9 +5,9 @@ import os
 import pandas as pd
 
 # sklearn.externals.joblib is deprecated in 0.21 and will be removed in 0.23. 
-# from sklearn.externals import joblib
+from sklearn.externals import joblib
 # Import joblib package directly
-import joblib
+#import joblib
 
 ## TODO: Import any additional libraries you need to define a model
 
